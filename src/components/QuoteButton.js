68a6430d90e6eeeeaa512a5542c320ee9845ad31
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-08-12 11:16:59 -0700

    initial commit

---
+++ src/components/QuoteButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  {Button}  from 'react-bootstrap'
+
+export default function QuoteButton() {
+  return (
+    <div>
+      <Button>
+        Submit
+      </Button>
+    </div>
+  )
+}
---
+++ src/components/Quotes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import QuoteButton from './QuoteButton'
+import { Card } from 'react-bootstrap'
+
+
+export default function Quotes() {
+  return (
+    <div>
+      <Card className="m-5 w-a">
+        <Card.Body>
+
+        </Card.Body>
+      </Card>
+      <QuoteButton />
+    </div>
+  )
+}
