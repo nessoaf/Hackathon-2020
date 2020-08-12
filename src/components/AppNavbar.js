e678a59867b0343ec58735616dbc422cb618935b
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-08-12 15:42:25 -0700

    appnav change

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -4,13 +4,12 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 export default function AppNavbar() {
   return (
     <Navbar bg="light" expand="lg" className="">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/create">Create</Nav.Link>
-          <Nav.Link href="/profile">Profile</Nav.Link>
+        <div className='navbar-brand-centered'>
+          <Nav.Link className='title' href="/">InspoME</Nav.Link>
+        </div>
+        <Nav className="mr-auto">
         </Nav>
         <Nav.Link className='content' href="/create">Create</Nav.Link>
         <Nav.Link className='content' href="/profile">Profile</Nav.Link>
