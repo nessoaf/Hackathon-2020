81df52273dc5c3b2a3c46c98148cd7eda2eec5c7
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-08-12 18:12:28 -0700

    css

--- src/create/ImageCreated.js
+++ src/create/ImageCreated.js
@@ -16,7 +16,7 @@ export default function ImageCreated() {
             Dropdown 
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item className href="#/action-1">Action</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
           </Dropdown.Menu>
