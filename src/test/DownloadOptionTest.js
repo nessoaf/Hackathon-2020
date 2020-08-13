f0938b514d4e6672bd7b7074b6bed6eaa8785f89
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-08-13 12:26:56 -0700

    misc changes and adjustment of nav

---
+++ src/test/DownloadOptionTest.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default function DownloadOptionTest() {
+  return (
+    <div className="download-container"> 
+      <div className="image-container">hi
+        <img className='icon content' src="./img/facebook1.png" />hi
+        <img className='icon content' src="./img/instagram.png" />hi
+        <img className='icon content' src="./img/pinterest.png" />hi
+        <img className='icon content' src="./img/twitter1.png" />hi
+      </div>
+    </div>
+  ) 
+}
