b4d66acb6ba9b5b5858895fd04ee0f15e73776a2
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-08-12 20:42:58 -0700

    cssssssssss

--- src/create/DropdownOption.js
+++ src/create/DropdownOption.js
@@ -21,11 +21,11 @@ export default function DropdownOption() {
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Abril Fatface</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Dancing Script</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Monoton</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Press Start</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Shadows Into Light</Dropdown.Item>
+          <Dropdown.Item className='fatface' href="#/action-1">Abril Fatface</Dropdown.Item>
+          <Dropdown.Item className='dancing' href="#/action-2">Dancing Script</Dropdown.Item>
+          <Dropdown.Item className='monoton' href="#/action-3">Monoton</Dropdown.Item>
+          <Dropdown.Item className='start2p' href="#/action-3">Press Start</Dropdown.Item>
+          <Dropdown.Item className='shadow' href="#/action-3">Shadows Into Light</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
       <Dropdown className="m-3 fonts">
