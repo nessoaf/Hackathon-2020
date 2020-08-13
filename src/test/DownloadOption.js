f0884ba447e7c1e3162d7fbdf4e74b3197c5d1a6
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-08-13 10:29:59 -0700

    added test branch

---
+++ src/test/DownloadOption.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default function DownloadOption() {
+  return (
+    <div className="download-container"> 
+      <div className="image-container">
+        <img className='icon content' src="./img/facebook1.png" />
+        <img className='icon content' src="./img/instagram.png" />
+        <img className='icon content' src="./img/pinterest.png" />
+        <img className='icon content' src="./img/twitter1.png" />
+      </div>
+    </div>
+  ) 
+}
---
+++ src/test/ImageCreated.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import  {Button}  from 'react-bootstrap'
+import { Dropdown } from 'react-bootstrap'
+
+export default function ImageCreated() {
+  return (
+    <div className="imagecreated-container">
+      <Card className="ml-5 mr-5 mb-4 mt-5 w-80">
+        <img className='image' src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/inspirational-quotes-william-james-1562000241.png?crop=1xw:1xh;center,top&resize=980:*" />
+      </Card>
+      <div className="button-container">
+      <Button className="m-3 content">Create</Button>
+        <Dropdown className="m-3 border-0">
+          <Dropdown.Toggle className="border-0 content" id="dropdown-basic">
+            Export 
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1" className="content">Small</Dropdown.Item>
+          <Dropdown.Item href="#/action-2" className="content">Medium</Dropdown.Item>
+          <Dropdown.Item href="#/action-3" className="content">Large</Dropdown.Item>
+          <Dropdown.Item href="#/action-3" className="content">Vector</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  )
+}
